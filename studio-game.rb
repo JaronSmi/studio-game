b9621e0a1ef15a98adddfb2b644151db6df831fa
JaronSmi <smithjaron5@gmail.com>
2024-02-29 08:14:47 -0600

    string, variable, interpolation exercises

--- studio-game.rb
+++ studio-game.rb
@@ -5,3 +5,17 @@ greeting = "Welcome!"
 end
 
 puts Time.now
+
+name1 = "larry"
+name2 = "moe"
+name3 = "curly"
+
+health1 = 60
+health2 = 45
+health3 = 90
+
+puts "#{name1}'s health is #{health1}"
+puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+
+
+
