0814194283a976a26ac46e1cb440b98952045816
JaronSmi <smithjaron5@gmail.com>
2024-02-29 09:21:46 -0600

    learning methods to modify string objects

--- studio-game.rb
+++ studio-game.rb
@@ -7,15 +7,23 @@ end
 puts Time.now
 
 name1 = "larry"
-name2 = "moe"
-name3 = "curly"
+name2 = "curly"
+name3 = "moe"
+name4 = "shemp"
 
 health1 = 60
-health2 = 45
-health3 = 90
+health2 = 125
+health3 = 100
+health4 = 90
 
-puts "#{name1}'s health is #{health1}"
 puts "Players: \n\t#{name1}\n\t#{name2}\n\t#{name3}"
+puts "#{name1.capitalize} has a health of #{health1}"
+puts "#{name2.upcase} has a health of #{health2}"
+puts "#{name3.capitalize} has a health of #{health3}".center(50, '*')
+puts "#{name4.capitalize.ljust(35, ".")} #{health4} health"
 
 
 
+#experimenting with pointing vairables to eachother
+health2 = health1
+puts "\n\n\n#{name2.upcase} has a health of #{health2}"
