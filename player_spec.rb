9e117c8a9e774b60bb32d81c2c21c1c72769911f
JaronSmi <smithjaron5@gmail.com>
2024-03-05 09:26:37 -0600

    add unit testing around player strength

--- player_spec.rb
+++ player_spec.rb
@@ -2,42 +2,58 @@ require_relative 'player'
 
 describe Player do
 
+    before do
+        @player = Player.new("larry", 150)
+        @initial_health = @player.health
+    end
+
     #examples
     it "has a capitalized name" do
-        player = Player.new("larry", 150)
-
-        player.name.should == "Larry"
+        @player.name.should == "Larry"
     end
 
     it "has an initial health" do
-        player = Player.new("larry", 150)
-        player.health.should == 150
+        @player.health.should == 150
     end
 
     it "has a string representation" do
-        player = Player.new("larry", 150)
-        player.to_s.should == "I'm Larry with a health of 150 and a score of 155"
+        @player.to_s.should == "I'm Larry with a health of 150 and a score of 155"
     end
 
     it "computes a score as the sum of its health and length of name" do
-        player = Player.new("larry", 150)
-
-        player.score.should == (150 + 5)
+        @player.score.should == (150 + 5)
     end
 
     it "increases health by 15 when w00ted" do
-        initial_health = 150
-        player = Player.new("larry", initial_health)
-        player.w00t
+        @player.w00t
       
-        player.health.should == initial_health + 15
+        @player.health.should == @initial_health + 15
     end
 
     it "decreases health by 10 when blammed" do
-        initial_health = 150
-        player = Player.new("larry", initial_health)
-        player.blam
+        @player.blam
 
-        player.health.should == initial_health - 10
+        @player.health.should == @initial_health - 10
     end
+
+    context "with a health greater than 100" do
+        before do
+            @player = Player.new("larry", 150)
+        end
+
+        it "is strong" do
+            @player.should be_strong
+        end
+    end
+
+    context "with a health less than 100" do
+        before do
+            @player = Player.new("larry", 90)
+        end
+
+        it "is not strong" do
+            @player.should_not be_strong
+        end
+    end
+    
 end
