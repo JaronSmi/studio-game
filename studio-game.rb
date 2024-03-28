898abe94f89a0b98fe92ccb2a2caf01f35579b33
JaronSmi <smithjaron5@gmail.com>
2024-03-28 10:02:41 -0500

    Read file from command line and clean up unnecessary comments

--- studio-game.rb
+++ studio-game.rb
@@ -1,20 +1,15 @@
 require_relative "game.rb"
 
-#instantiate players
+
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
-#instantiate game
+
 knuckleheads = Game.new("Knuckleheads")
 
-#add players to game
-# knuckleheads.add_player(player1)
-# knuckleheads.add_player(player2)
-# knuckleheads.add_player(player3)
-knuckleheads.load_players("players.csv")
-
+knuckleheads.load_players(ARGV.shift || "players.csv")
 puts "".ljust(50,"-")
-#play game
+
 loop do
     puts "\nHow many game rounds? ('quit' to exit)"
     answer = gets.chomp.downcase
