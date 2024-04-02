f61d940134231ba29197f9dbad00f4c2fc80b82d
JaronSmi <smithjaron5@gmail.com>
2024-04-02 10:19:30 -0500

    refactor clumsy player

--- clumsy_player.rb
+++ clumsy_player.rb
@@ -2,8 +2,8 @@ require_relative "player"
 
 class ClumsyPlayer < Player
     def found_treasure(treasure)
-        points = treasure.points/2
-        @found_treasures[treasure.name] += points
+        damaged_treasure = Treasure.new(treasure.name, treasure.points / 2.0)
+        super(damaged_treasure)
     end
 end
 
